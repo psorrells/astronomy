341a46f9bac23ef251814aace6b1f64099dc8cf7
psorrells <pamelan.sorrells@gmail.com>
2022-04-11 20:44:55 -0500

    added input to html and getPOD function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=wHzsaYP8IpCpR6pWuWPl9LQYDhR30PUxfFnFg08V&date=${year}-${month}-${day}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => new Error(`failed fetch: ${err}`))
+
+function getPOD() {
+    const year = document.querySelector('#year').value 
+    const month = document.querySelector('#month').value
+    const day = document.querySelector('day').value
+
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=wHzsaYP8IpCpR6pWuWPl9LQYDhR30PUxfFnFg08V&date=${year}-${month}-${day}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => new Error(`failed fetch: ${err}`))
+}

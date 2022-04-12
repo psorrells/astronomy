95a25ff6b171564d3dfb6434b0020df4dcb536a0
psorrells <pamelan.sorrells@gmail.com>
2022-04-11 20:52:00 -0500

    added event listener

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 
+document.querySelector('#getPic').addEventListener('click',getPOD)
+
 
 function getPOD() {
-    const year = document.querySelector('#year').value 
-    const month = document.querySelector('#month').value
-    const day = document.querySelector('day').value
+    const year = document.querySelector('#year').value
+    const month = document.querySelector('#month').value.padStart(2,'0')
+    const day = document.querySelector('#day').value.padStart(2,'0')
 
     fetch(`https://api.nasa.gov/planetary/apod?api_key=wHzsaYP8IpCpR6pWuWPl9LQYDhR30PUxfFnFg08V&date=${year}-${month}-${day}`)
         .then(res => res.json())

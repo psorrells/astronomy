5ea1c5484a81f59fd10eca1ea704446463b0fd32
psorrells <pamelan.sorrells@gmail.com>
2022-04-11 21:16:22 -0500

    collects image and video data

--- js/main.js
+++ js/main.js
@@ -8,8 +8,23 @@ function getPOD() {
     const month = document.querySelector('#month').value.padStart(2,'0')
     const day = document.querySelector('#day').value.padStart(2,'0')
 
+    let media = document.querySelector('.media')
+
     fetch(`https://api.nasa.gov/planetary/apod?api_key=wHzsaYP8IpCpR6pWuWPl9LQYDhR30PUxfFnFg08V&date=${year}-${month}-${day}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            document.querySelector('h2').innerText = data.title
+            document.querySelector('h3').innerText = data.explanation
+            media.childNodes.forEach(child => child.remove())
+            if (data.media_type === 'image') {
+                img = document.createElement('img')
+                img.src = data.url
+                media.appendChild(img)
+            } else if (data.media_type === 'video') {
+                img = document.createElement('iframe')
+                img.src = data.url
+                media.appendChild(img)
+            }
+        })
         .catch(err => new Error(`failed fetch: ${err}`))
 }
